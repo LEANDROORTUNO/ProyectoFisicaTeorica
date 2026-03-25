d361a895a104808f93b04a7c2c2a797b52271250
Leandro Ortuño <menprogramacion@gmail.com>
2026-03-24 23:31:14 -0400

    Ultimas Correcciones

--- CodigoProyecto/Persecucion.py
+++ CodigoProyecto/Persecucion.py
@@ -12,21 +12,21 @@ class ProyectoPersecucion(Scene):
         vehiculo2 = res.getVehiculo2()
 
         #obejtos visuales(preguntar como se usa label, carro, ejes)
-        ejes = NumberLine(x_range=[0, 500, 100], length=10, include_numbers=True).shift(DOWN)
+        ejes = NumberLine(x_range=[0, 500, 100], length=10, include_numbers=True).shift(DOWN * 2)
         #imagenes de autos
         try:
             carro_a = ImageMobject("../ImagenesProyecto/AutoAzul.png")
-            carro_a.scale(0.4)
+            carro_a.scale(0.7)
             carro_b = ImageMobject("../ImagenesProyecto/AutoRojo.png")
-            carro_b.scale(0.4)
+            carro_b.scale(0.7)
 
         except FileNotFoundError:
             print("Error: No se encontraron las imágenes PNG. Usando puntos de respaldo.")
             carro_a = Dot(color=BLUE)
             carro_b = Dot(color=RED)
 
-        carro_a.move_to(ejes.n2p(vehiculo1.getPosInicial())).shift(UP*0.5)
-        carro_b.move_to(ejes.n2p(vehiculo2.getPosInicial())).shift(UP*0.5)
+        carro_a.move_to(ejes.n2p(vehiculo1.getPosInicial())).shift(UP*0.7)
+        carro_b.move_to(ejes.n2p(vehiculo2.getPosInicial())).shift(UP*0.7)
 
         label_a = Text("Auto A", font_size=24).next_to(carro_a, UP)
         label_b = Text("Auto B", font_size=24).next_to(carro_b, UP) 
@@ -40,8 +40,8 @@ class ProyectoPersecucion(Scene):
 
         # Actualizadores (Mantenemos la física unida a lo visual)(preguntar)
         # Busca los add_updater dentro de construct()
-        carro_a.add_updater(lambda m: m.move_to(ejes.n2p(vehiculo1.getPosicionFinal(reloj.get_value()))).shift(UP*0.2)) 
-        carro_b.add_updater(lambda m: m.move_to(ejes.n2p(vehiculo2.getPosicionFinal(reloj.get_value()))).shift(UP*0.2)) 
+        carro_a.add_updater(lambda m: m.move_to(ejes.n2p(vehiculo1.getPosicionFinal(reloj.get_value()))).shift(UP*0.7)) 
+        carro_b.add_updater(lambda m: m.move_to(ejes.n2p(vehiculo2.getPosicionFinal(reloj.get_value()))).shift(UP*0.7)) 
         label_a.add_updater(lambda m: m.next_to(carro_a, UP))
         label_b.add_updater(lambda m: m.next_to(carro_b, UP))
 
